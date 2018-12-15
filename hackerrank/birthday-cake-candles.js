f02b89202372b55b1d3723386424bd05186fd83a
Jinseop Lee <js428118@gmail.com>
2018-12-16 03:49:28 +0900

    reduce

--- hackerrank/birthday-cake-candles.js
+++ hackerrank/birthday-cake-candles.js
@@ -1,24 +1,9 @@
 function birthdayCakeCandles(ar) {
-
-    let tmp = 0;
-    for(let item in ar) {
-        if(tmp < ar[item]) {
-            tmp = ar[item];
-        }
-    }
-
-    let count = 0;
-    for(let item in ar) {
-        if(tmp == ar[item]) {
-            count++;
-        }
-    }
-
-    return count;
+    let max = Math.max(...ar);
+    return ar.reduce((count, i) => i === max ? count+1 : count ,0);
 }
 
 const arCount = 4;
 const ar = "3 2 1 3".split(' ').map(arTemp => parseInt(arTemp, 10));
-
-birthdayCakeCandles(ar);
+console.log(birthdayCakeCandles(ar));
 
