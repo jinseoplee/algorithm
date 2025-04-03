185632d85dc59db7361d9c5fd51e29aba3457fd9
jinseoplee <jjjp9966@gmail.com>
2025-04-03 18:09:50 +0900

    [level 2] Title: 올바른 괄호, Time: 6.21 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,13 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            else if (count == 0) return false;
+            else count--;
+        }
+        
+        return count == 0;
+    }
+}
