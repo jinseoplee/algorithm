c8a703840e1cc91f7003cf4869b6f6752e3ce239
jinseoplee <jjjp9966@gmail.com>
2025-04-08 01:39:08 +0900

    [Silver V] Title: 단어 정렬, Time: 232 ms, Memory: 28648 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -11,23 +9,26 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 
 		int n = Integer.parseInt(br.readLine());
-
-		Set<String> set = new HashSet<>();
-		while (n-- > 0) {
-			String w = br.readLine();
-			set.add(w);
+		String[] arr = new String[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = br.readLine();
 		}
 
-		List<String> list = new ArrayList<>(set);
-		list.sort((o1, o2) -> {
-			if (o1.length() == o2.length()) {
-				return o1.compareTo(o2);
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.length() == o2.length()) {
+					return o1.compareTo(o2);
+				}
+				return o1.length() - o2.length();
 			}
-			return o1.length() - o2.length();
 		});
 
-		for (String w : list) {
-			sb.append(w).append("\n");
+		sb.append(arr[0]).append("\n");
+		for (int i = 1; i < n; i++) {
+			if (!arr[i].equals(arr[i - 1])) {
+				sb.append(arr[i]).append("\n");
+			}
 		}
 		System.out.println(sb);
 	}
