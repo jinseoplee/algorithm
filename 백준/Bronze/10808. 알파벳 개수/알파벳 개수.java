c46b85e56d3d54b3ca5d40ad4f55df6aec903914
jinseoplee <jjjp9966@gmail.com>
2025-07-10 08:07:44 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		String str = br.readLine();
+
+		int[] freq = new int[26];
+		for (int i = 0; i < str.length(); i++) {
+			freq[str.charAt(i) - 'a']++;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			sb.append(freq[i]).append(" ");
+		}
+
+		System.out.println(sb);
+	}
+}
